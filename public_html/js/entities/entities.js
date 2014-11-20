457d849b5e236fc1cfa00334e3e3db2d3e04aefc
javier alvarez <javi_612@yahoo.com>
2014-11-20 14:03:14 -0800

    mario

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -30,6 +30,9 @@ game.PlayerEntity = me.Entity.extend({
             this.body.vel.x = 0;
         }
         
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        
         if(this.body.vel.x !== 0){
             if(!this.renderable.isCurrentAnimation("smallWalk")) {
                 this.renderable.setCurrentAnimation("smallWalk");
@@ -39,9 +42,12 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.setCurrentAnimation("idle");            
         }
     
-        this.body.update(delta);
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    collideHandler: function(response){
+        
     }
     
 });
@@ -49,11 +55,13 @@ game.PlayerEntity = me.Entity.extend({
 game.LevelTrigger = me.Entity.extend({
     init: function(x, y, settings){
         this._super(me.Entity, 'init', [x, y, settiings]);
-        
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
     },
     
     onCollision: function(){
-        
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadlevel(this.level);
     }
     
 });
