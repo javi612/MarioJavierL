6524ecf70399a71512f05d6405a50314043d7d8c
javier alvarez <javi_612@yahoo.com>
2014-12-09 13:08:48 -0800

    finished

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,13 +3,14 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		var titleImage = (new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
-                me.game.world.addChild(titleImage, 1);
+//		var titleImage = (new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
+//                me.game.world.addChild(titleImage, 1);
+                me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
                 me.input.bindKey(me.input.KEY.ENTER, "start"); 
                 
                 me.game.world.addChild(new (me.Renderable.extend ({
                     init: function(){
-                        this._super (me. Renderable, 'init', [510, 30, me.game.viewport.width, me.game.veiwport.height]);
+                        this._super (me. Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
                         this.font = new me.Font("Arial", 46, "white");
                         
                     },
@@ -34,6 +35,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKEY(me.input.KEY.ENTER);
+		me.input.unbindKey(me.input.KEY.ENTER);
 	}
 });
