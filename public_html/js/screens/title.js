e0c0def79c3772844c32280e0d943f4f7bec1cfd
javier alvarez <javi_612@yahoo.com>
2014-12-08 19:40:58 -0800

    HELP

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,30 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		var titleImage = (new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
+                me.game.world.addChild(titleImage, 1);
+                me.input.bindKey(me.input.KEY.ENTER, "start"); 
+                
+                me.game.world.addChild(new (me.Renderable.extend ({
+                    init: function(){
+                        this._super (me. Renderable, 'init', [510, 30, me.game.viewport.width, me.game.veiwport.height]);
+                        this.font = new me.Font("Arial", 46, "white");
+                        
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+                        this.font.draw(renderer.getContext(), "Press Enter To Play", 250, 530);
+                    }
+                    
+                })));
+                                
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+                    if(action === "start"){
+                        me.state.change(me.state.PLAY);
+                    }
+                });
 	},
 	
 	
@@ -11,6 +34,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKEY(me.input.KEY.ENTER);
 	}
 });
